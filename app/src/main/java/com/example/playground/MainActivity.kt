58fdb44a5d14daa481793c65b7bc37f1422222f3
taeyoung <rlaxodud911@gmail.com>
2024-07-30 18:58:23 +0900

    :sparkles: feature: GoogleMap 예제 코드

--- app/src/main/java/com/example/playground/MainActivity.kt
+++ app/src/main/java/com/example/playground/MainActivity.kt
@@ -1,20 +1,143 @@
 package com.example.playground
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnSuccessListener
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mGoogleMap: GoogleMap
+    private lateinit var locationPermission: ActivityResultLauncher<Array<String>>
+
+    //위치 서비스가 gps를 사용해서 위치를 확인
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    //위치 값 요청에 대한 갱신 정보를 받는 변수
+    private lateinit var locationCallback: LocationCallback
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 권한 확인
+        locationPermission = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { results ->
+            if (results.all { it.value }) {
+                // 권한이 있다면 map과 연결
+                // OnMapReadyCallback에 의해 맵이 준비되면 onMapReady 콜백이 불린다.
+                (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment).getMapAsync(
+                    this
+                )
+            } else { //문제가 발생했을 때
+                Toast.makeText(this, "권한 승인이 필요합니다.", Toast.LENGTH_LONG).show()
+            }
         }
+
+        // 권한이 없다면, 권한 요청
+        locationPermission.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        )
+    }
+
+
+    // 지도 객체를 이용할 수 있는 상황이 될 때
+    override fun onMapReady(p0: GoogleMap) {
+
+        val seoul = LatLng(37.566610, 126.978403)
+        mGoogleMap = p0
+        mGoogleMap.mapType = GoogleMap.MAP_TYPE_NORMAL // default 노말 생략 가능
+        mGoogleMap.apply {
+            // 서울 시청에 마커 하나 찍어놓음
+            val markerOptions = MarkerOptions()
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+            markerOptions.position(seoul)
+            markerOptions.title("서울시청")
+            markerOptions.snippet("Tel:01-120")
+            addMarker(markerOptions)
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        updateLocation()
+    }
+
+    // 위치를 계속 받아온다.
+    private fun updateLocation() {
+
+        val locationRequest = LocationRequest.create().apply {
+            interval = 1000
+            fastestInterval = 500
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        locationCallback = object : LocationCallback() {
+            // 1초에 한번씩 변경된 위치 정보가 onLocationResult 으로 전달된다.
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult?.let {
+                    for (location in it.locations) {
+                        Log.d("위치정보", "위도: ${location.latitude} 경도: ${location.longitude}")
+                        // 1초마다 현재 위치에 마커 찍음 + 현재 위치로 카메라 이동
+                        setLastLocation(location) //계속 실시간으로 위치를 받아오고 있기 때문에 맵을 확대해도 다시 줄어든다.
+                    }
+                }
+            }
+        }
+        // 권한 처리
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        // requestLocationUpdates에 만들어둔 locationRequest와 locationCallback 등록
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest, locationCallback,
+            Looper.myLooper()!!
+        )
+    }
+
+    fun setLastLocation(lastLocation: Location) {
+        val LATLNG = LatLng(lastLocation.latitude, lastLocation.longitude)
+
+        val makerOptions = MarkerOptions().position(LATLNG).title("나 여기 있어용~")
+        val cameraPosition = CameraPosition.Builder().target(LATLNG).zoom(15.0f).build()
+
+        mGoogleMap.addMarker(makerOptions)
+        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
 }
